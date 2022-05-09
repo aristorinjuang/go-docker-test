e73cfee2c1956b578eeaa011f3320c32e093f6db
Aristo Rinjuang <aristorinjuang@gmail.com>
2022-05-10 00:06:54 +0700

    FEATURE: create a folder, file, and log

--- main.go
+++ main.go
@@ -4,9 +4,14 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	os.Mkdir("tmp", os.ModePerm) // Test create a folder
+	os.Create("tmp/tmp")         // Test create a file
+
+	log.Println("The app starts at 8080.")
 	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
 		io.WriteString(w, "Hello World!")
 	})
